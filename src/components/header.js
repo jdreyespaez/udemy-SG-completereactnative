794d852adc2f7d32d9d7731c5ecccb1f4ec12735
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2018-12-20 15:14:21 -0500

    Same 3 steps, slightly change at the end 'making the component available'

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+// First step: import libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Second step: create the component
+// The name of the function will be the same as the component
+const Header = () => {
+    return <Text>Albums!</Text>;
+};
+
+
+// Third step: make the component available
+// Using ES6 syntax
+export default Header;
