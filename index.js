afe0f24a9fcd76dcf650b55b6b4cab6b40f40052
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2018-12-20 15:13:21 -0500

    Importing and consuming the component

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Import a library to create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
 
 // Create a component
 const App = () => {
     return (
-        <Text>Some Text</Text>
+        <Header />
     );
 };
 
 // Render it to the device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+// App as the root component is the only one using AppRegistry
+AppRegistry.registerComponent('albums', () => App);
