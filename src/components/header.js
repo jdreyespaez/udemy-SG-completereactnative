16fdeb978abc80fa85b4852de2bf6dd1acf9bc8f
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2018-12-20 15:18:26 -0500

    The styles are defined as an object and then used like props

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,18 @@ import { Text } from 'react-native';
 // Second step: create the component
 // The name of the function will be the same as the component
 const Header = () => {
-    return <Text>Albums!</Text>;
+    const { textStyle } = styles;
+
+    // style is a prop
+    return <Text style={textStyle}>Albums!</Text>;
 };
 
+// By convention styles will go as an object after the component
+const styles = {
+    textStyle: {
+        fontSize: 20
+    }
+};
 
 // Third step: make the component available
 // Using ES6 syntax
