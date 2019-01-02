c7a2a8b4b8b9ce236564c5158688309fd7528a42
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-02 04:55:43 -0500

    Adding style to thumbnail image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    // Destructuring the data
+    const { title, artist, thumbnail_image } = album;
+
     return (
         <Card>
             <CardSection>
-                <View></View>
+                <View>
+                    <Image 
+                        style={styles.thumbnailStyle}
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
                 <View style={styles.headerContentStyle} >
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
                 </View>
             </CardSection>
         </Card>
@@ -20,7 +28,11 @@ const AlbumDetail = (props) => {
 const styles = {
     headerContentStyle: {
         flexDirection: 'column',
-        justifyContent: 'space-around' 
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
     }
 };
 
