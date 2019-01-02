6f26622d7bc0967212ecbb991e6c0bf68fcd77b9
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-01-02 10:20:13 -0500

    Using Linking from APIS to direct to URL, sugin .openURL()

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -10,7 +10,9 @@ const AlbumDetail = ({ album }) => {
         title, 
         artist, 
         thumbnail_image, 
-        image } = album;
+        image,
+        url 
+    } = album;
     const { 
         thumbnailStyle, 
         headerContentStyle, 
@@ -43,7 +45,7 @@ const AlbumDetail = ({ album }) => {
             </CardSection>
             {/* Button section */}
             <CardSection>
-                <Button cuandoPresionado={() => console.log(title)} />
+                <Button cuandoPresionado={() => Linking.openURL(url)} />
             </CardSection>
         </Card>
     );
